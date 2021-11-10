10bcc173286edc24cefa6039085bb2e7c982fc07
piotrszymanski133 <63449627+piotrszymanski133@users.noreply.github.com>
2021-11-10 18:37:50 +0100

    Create basic rabbitmq consumer Save consumed messages in mongoDB

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using RabbitMQ.Client;
+
+namespace si_net_project_consumer
+{
+    class Program
+    {
+        private const string ConnectionString = "mongodb://root:example@localhost:27017/";
+        private const string DatabaseName = "SI_17555";
+        private const string Host = "localhost";
+        private const string TemperatureCollection = "temperature";
+        
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = Host };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: TemperatureCollection,
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
+                    
+                    var temperatureConsumer = new Consumer(channel, ConnectionString, DatabaseName, TemperatureCollection);
+                    channel.BasicConsume(TemperatureCollection, false, temperatureConsumer);
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
