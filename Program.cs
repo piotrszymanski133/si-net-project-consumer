980c14d6f41d759ec30448c29550c7f5a5218b29
jakub_czarnecki <jakub.piotr.czarnecki@gmail.com>
2021-11-30 22:34:11 +0100

    Quickfix

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace si_net_project_consumer
 {
     class Program
     {
-        private const string ConnectionString = "mongodb://root:student@mlocalhost:27017/";
+        private const string ConnectionString = "mongodb://root:student@localhost:27017/";
         private const string DatabaseName = "SI_17555";
         private const string Host = "rabbitmq";
         private const string TemperatureCollection = "temperature";
